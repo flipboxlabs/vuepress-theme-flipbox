7a3b4eb7887bcb894f406b2fa47d4d9efc631bb8
Doug St. John <3673451+dougstjohn@users.noreply.github.com>
2018-11-05 19:13:43 -0700

    default vuepress eject

--- Layout.vue
+++ Layout.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="theme-container"
-       :class="{
+    :class="{
       'no-navbar': !shouldShowNavbar,
       'sidebar-open': isSidebarOpen,
       'no-sidebar': !shouldShowSidebar
     }"
-       @touchstart="onTouchStart"
-       @touchend="onTouchEnd">
+    @touchstart="onTouchStart"
+    @touchend="onTouchEnd">
     <Navbar v-if="shouldShowNavbar" @toggle-sidebar="toggleSidebar"/>
     <Sidebar :items="sidebarItems" @toggle-sidebar="toggleSidebar"/>
     <div class="custom-layout" v-if="$page.frontmatter.layout">
@@ -18,136 +18,136 @@
 </template>
 
 <script>
-    import Vue from 'vue'
-    import nprogress from 'nprogress'
-    import Home from './Home.vue'
-    import Navbar from './Navbar.vue'
-    import Page from './Page.vue'
-    import Sidebar from './Sidebar.vue'
-    import { pathToComponentName, getTitle, getLang } from '@app/util'
-    import { resolveSidebarItems } from './util'
+import Vue from 'vue'
+import nprogress from 'nprogress'
+import Home from './Home.vue'
+import Navbar from './Navbar.vue'
+import Page from './Page.vue'
+import Sidebar from './Sidebar.vue'
+import { pathToComponentName, getTitle, getLang } from '@app/util'
+import { resolveSidebarItems } from './util'
 
-    export default {
-        components: { Home, Page, Sidebar, Navbar },
-        data () {
-            return {
-                isSidebarOpen: false
-            }
-        },
-
-        computed: {
-            shouldShowNavbar () {
-                const { themeConfig } = this.$site
-                return (
-                    this.$site.title ||
-                    themeConfig.logo ||
-                    themeConfig.repo ||
-                    themeConfig.nav
-                )
-            },
-            shouldShowSidebar () {
-                const { themeConfig } = this.$site
-                const { frontmatter } = this.$page
-                return (
-                    !frontmatter.layout &&
-                    !frontmatter.home &&
-                    frontmatter.sidebar !== false && (
-                        frontmatter.sidebar === 'auto' ||
-                        themeConfig.sidebar
-                    )
-                )
-            },
-            sidebarItems () {
-                return resolveSidebarItems(
-                    this.$page,
-                    this.$route,
-                    this.$site
-                )
-            }
-        },
-
-        created () {
-            if (this.$ssrContext) {
-                this.$ssrContext.title = getTitle(this.$site, this.$page)
-                this.$ssrContext.lang = getLang(this.$page)
-            }
-        },
-
-        mounted () {
-            // update title / meta tags
-            this.currentMetaTags = []
-            const updateMeta = () => {
-                document.title = getTitle(this.$site, this.$page)
-                document.documentElement.lang = getLang(this.$page)
-                this.currentMetaTags = updateMetaTags(this.$page, this.currentMetaTags)
-            }
-            this.$watch('$page', updateMeta)
-            updateMeta()
-
-            // configure progress bar
-            nprogress.configure({ showSpinner: false })
-
-            this.$router.beforeEach((to, from, next) => {
-                if (to.path !== from.path && !Vue.component(pathToComponentName(to.path))) {
-                    nprogress.start()
-                }
-                next()
-            })
-
-            this.$router.afterEach(() => {
-                nprogress.done()
-                this.isSidebarOpen = false
-            })
-        },
-
-        beforeDestroy () {
-            updateMetaTags(null, this.currentMetaTags)
-        },
-
-        methods: {
-            toggleSidebar (to) {
-                this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
-            },
-            // side swipe
-            onTouchStart (e) {
-                this.touchStart = {
-                    x: e.changedTouches[0].clientX,
-                    y: e.changedTouches[0].clientY
-                }
-            },
-            onTouchEnd (e) {
-                const dx = e.changedTouches[0].clientX - this.touchStart.x
-                const dy = e.changedTouches[0].clientY - this.touchStart.y
-                if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
-                    if (dx > 0 && this.touchStart.x <= 80) {
-                        this.toggleSidebar(true)
-                    } else {
-                        this.toggleSidebar(false)
-                    }
-                }
-            }
-        }
+export default {
+  components: { Home, Page, Sidebar, Navbar },
+  data () {
+    return {
+      isSidebarOpen: false
     }
+  },
 
-    function updateMetaTags (page, current) {
-        if (current) {
-            current.forEach(c => {
-                document.head.removeChild(c)
-            })
-        }
-        const data = page && page.frontmatter.meta
-        if (data) {
-            return data.map(m => {
-                const tag = document.createElement('meta')
-                Object.keys(m).forEach(key => {
-                    tag.setAttribute(key, m[key])
-                })
-                document.head.appendChild(tag)
-                return tag
-            })
-        }
+  computed: {
+    shouldShowNavbar () {
+      const { themeConfig } = this.$site
+      return (
+        this.$site.title ||
+        themeConfig.logo ||
+        themeConfig.repo ||
+        themeConfig.nav
+      )
+    },
+    shouldShowSidebar () {
+      const { themeConfig } = this.$site
+      const { frontmatter } = this.$page
+      return (
+        !frontmatter.layout &&
+        !frontmatter.home &&
+        frontmatter.sidebar !== false && (
+          frontmatter.sidebar === 'auto' ||
+          themeConfig.sidebar
+        )
+      )
+    },
+    sidebarItems () {
+      return resolveSidebarItems(
+        this.$page,
+        this.$route,
+        this.$site
+      )
     }
+  },
+
+  created () {
+    if (this.$ssrContext) {
+      this.$ssrContext.title = getTitle(this.$site, this.$page)
+      this.$ssrContext.lang = getLang(this.$page)
+    }
+  },
+
+  mounted () {
+    // update title / meta tags
+    this.currentMetaTags = []
+    const updateMeta = () => {
+      document.title = getTitle(this.$site, this.$page)
+      document.documentElement.lang = getLang(this.$page)
+      this.currentMetaTags = updateMetaTags(this.$page, this.currentMetaTags)
+    }
+    this.$watch('$page', updateMeta)
+    updateMeta()
+
+    // configure progress bar
+    nprogress.configure({ showSpinner: false })
+
+    this.$router.beforeEach((to, from, next) => {
+      if (to.path !== from.path && !Vue.component(pathToComponentName(to.path))) {
+        nprogress.start()
+      }
+      next()
+    })
+
+    this.$router.afterEach(() => {
+      nprogress.done()
+      this.isSidebarOpen = false
+    })
+  },
+
+  beforeDestroy () {
+    updateMetaTags(null, this.currentMetaTags)
+  },
+
+  methods: {
+    toggleSidebar (to) {
+      this.isSidebarOpen = typeof to === 'boolean' ? to : !this.isSidebarOpen
+    },
+    // side swipe
+    onTouchStart (e) {
+      this.touchStart = {
+        x: e.changedTouches[0].clientX,
+        y: e.changedTouches[0].clientY
+      }
+    },
+    onTouchEnd (e) {
+      const dx = e.changedTouches[0].clientX - this.touchStart.x
+      const dy = e.changedTouches[0].clientY - this.touchStart.y
+      if (Math.abs(dx) > Math.abs(dy) && Math.abs(dx) > 40) {
+        if (dx > 0 && this.touchStart.x <= 80) {
+          this.toggleSidebar(true)
+        } else {
+          this.toggleSidebar(false)
+        }
+      }
+    }
+  }
+}
+
+function updateMetaTags (page, current) {
+  if (current) {
+    current.forEach(c => {
+      document.head.removeChild(c)
+    })
+  }
+  const data = page && page.frontmatter.meta
+  if (data) {
+    return data.map(m => {
+      const tag = document.createElement('meta')
+      Object.keys(m).forEach(key => {
+        tag.setAttribute(key, m[key])
+      })
+      document.head.appendChild(tag)
+      return tag
+    })
+  }
+}
 </script>
 
-<style src="prismjs/themes/prism-solarizedlight.css"></style>
+<style src="prismjs/themes/prism-tomorrow.css"></style>
 <style src="./styles/theme.styl" lang="stylus"></style>
